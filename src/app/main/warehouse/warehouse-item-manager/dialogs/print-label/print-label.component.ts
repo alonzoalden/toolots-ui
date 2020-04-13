f22786a172821c3542bd7c2a3ad909a510c7e470
alonzoalden <alonzoalden@gmail.com>
2020-04-13 10:54:54 -0700

    print item label

--- src/app/main/warehouse/warehouse-item-manager/dialogs/print-label/print-label.component.ts
+++ src/app/main/warehouse/warehouse-item-manager/dialogs/print-label/print-label.component.ts
@@ -1,5 +1,5 @@
 import { Component, Inject, ViewEncapsulation, OnInit, OnDestroy } from '@angular/core';
-import { FormControl, FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder } from '@angular/forms';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { WarehouseItemManagerService } from '../../warehouse-item-manager.service';
 import { takeUntil } from 'rxjs/operators';
@@ -22,7 +22,8 @@ export class PrintLabelDialogComponent implements OnInit, OnDestroy {
     private _unsubscribeAll: Subject<any>;
     isPrinting: boolean;
     printers: any[];
-
+    printerInterval: any;
+    selectedPrinter: any;
     objectKeys = Object.keys;
     dictPackingType = {
         4: 'LTL',
@@ -37,12 +38,6 @@ export class PrintLabelDialogComponent implements OnInit, OnDestroy {
         'KG'
     ];
 
-    /**
-     * Constructor
-     *
-     * @param {MatDialogRef<PrintLabelDialogComponent>} matDialogRef
-     * @param _data
-     */
     constructor(
         private _formBuilder: FormBuilder,
         public matDialogRef: MatDialogRef<PrintLabelDialogComponent>,
@@ -57,108 +52,68 @@ export class PrintLabelDialogComponent implements OnInit, OnDestroy {
                 null, null, null, null, null, null, null, null,
                 null, null, null, null, null, null, null, null, [], [], [])
             );
-        this.composeForm = this.createProductForm();
         this.printers = [];
 
     }
 
     ngOnInit(): void {
         this.printers = dymo.label.framework.getPrinters();
-
+        if (this.printers.length === 0) {
+            this._snackBar.openFromComponent(SnackbarComponent, {
+                duration: 0,
+                data: { type: 'error', message: `Error: 'No DYMO printers are installed. Install DYMO printers.'` },
+            });
+            console.log('No DYMO printers are installed. Install DYMO printers.');
+        }
         this.warehouseItemManagerService.onItemSelected
             .pipe(takeUntil(this._unsubscribeAll))
             .subscribe(selected => {
                 this.selected = selected;
-                if (selected.Data) {
-                    this.composeForm = this.createProductForm();
-                }
             });
+
+        this.printerInterval = setInterval(() => {
+            this.printers = dymo.label.framework.getPrinters();
+            if (this.selectedPrinter) {
+                this.isPrinterConnected();
+            }
+        }, 3000)
     }
     ngOnDestroy(): void {
         // Unsubscribe from all subscriptions
         this._unsubscribeAll.next();
         this._unsubscribeAll.complete();
+        clearInterval(this.printerInterval);
     }
 
-    // -----------------------------------------------------------------------------------------------------
-    // @ Public methods
-    // -----------------------------------------------------------------------------------------------------
-
-    createProductForm(): FormGroup {
-        return this._formBuilder.group({
-            Printer: [this.selected.ItemID],
-            Message: [this.selected.ItemName]
-        });
-    }
-
-    /**
-     * Toggle extra to fields
-     */
     toggleExtraToFields(): void {
         this.showExtraToFields = !this.showExtraToFields;
     }
-    print(): void {
-    }
-    // save(): void {
-    //     this.isSaving = true;
-    //     this.warehouseItemManagerService.editItemDimension(this.composeForm.value)
-    //         .subscribe(
-    //             data => {
-    //                 this.selected.Data = data;
-    //                 this.warehouseItemManagerService.onItemSelected.next(this.selected);
-    //                 this.matDialogRef.close(this.selected);
-    //             },
-    //             error => {
-    //                 this._snackBar.openFromComponent(SnackbarComponent, {
-    //                     data: { type: 'error', message: error },
-    //                     duration: 0,
-    //                 });
-    //                 this.isSaving = false;
-    //             }
-    //         );
-    // }
-
-
-
     // prints the label
     onPrint() {
-        const label = dymo.label.framework.openLabelFile("C://code/ItemLabel-1.label");
+        if (!this.isPrinterConnected) {
+            return;
+        }
+        const label = dymo.label.framework.openLabelFile("https://localhost:4200/assets/ItemLabel-1.label");
+        // const label = dymo.label.framework.openLabelFile("C://code/internal/src/assets/ItemLabel-1.label");
         // load image from url and store as Base64
-        //var image = dymo.label.framework.loadImageAsPngBase64("C://code/internal/src/assets/images/home-gear.png");
-        // overwrite image "Image" from XML label with loaded image
+        // var image = dymo.label.framework.loadImageAsPngBase64("C://code/internal/src/assets/images/home-gear.png");
         label.setObjectText("SKU", this.selected.VendorSKU);
-        label.setObjectText("Barcode", '0200022');
+        label.setObjectText("Barcode", this.selected.TPIN);
         label.setObjectText("TPIN", this.selected.TPIN);
-
-        // select printer to print on
-        // for simplicity sake just use the first LabelWriter printer
-        const printers = dymo.label.framework.getPrinters();
-        if (printers.length === 0) {
+        label.print(this.selectedPrinter);
+    }
+    isPrinterConnected(): boolean {
+        const connected = this.printers.find(
+            (printer) => this.selectedPrinter === printer.name && printer.isConnected
+        );
+        if (!connected) {
             this._snackBar.openFromComponent(SnackbarComponent, {
                 duration: 0,
-                data: { type: 'error', message: `Error: 'No DYMO printers are installed. Install DYMO printers.'` },
+                data: { type: 'error', message: `Error: '${this.selectedPrinter} has been disconnected.'` },
             });
-            console.log('No DYMO printers are installed. Install DYMO printers.');
+            this.selectedPrinter = null;
+            return;
         }
-
-        let printerName = '';
-        for (const printer of printers) {
-            // const printer = printers[i];
-            if (printer.printerType === 'LabelWriterPrinter') {
-                printerName = printer.name;
-                break;
-            }
-        }
-
-        if (printerName === '') {
-            this._snackBar.openFromComponent(SnackbarComponent, {
-                duration: 0,
-                data: { type: 'error', message: `Error: 'No DYMO printers are installed. Install DYMO printers.'` },
-            });
-            console.log('No DYMO printers are installed. Install DYMO printers.');
-        }
-
-        // finally print the label
-        label.print(printerName);
+        return !!connected;
     }
 }
